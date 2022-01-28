37a5fb35bc8d2271e20d77f06e9fc6791acf8993
Temi <stadeniy@cougarnet.uh.edu>
2022-01-28 12:57:07 -0600

    got the api working here

--- API_Project/API-build.py
+++ API_Project/API-build.py
@@ -5,16 +5,20 @@ app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
-def homepage(_):
-    dataset={"Page": "Home", "Message": "successfully loaded the Home page", "Timestamp":time.time}
+def homepage():
+    dataset={"Page": "Home", "Message": "successfully loaded the Home page", "Timestamp":time.time()}
     json_dump = json.dumps(dataset)
 
     return json_dump
 
 
-@app.route('/user', methods=['GET'])
-def homepage(_):
-    dataset={"Page": "Home", "Message": "successfully loaded the Home page", "Timestamp":time.time}
+@app.route('/user/', methods=['GET'])
+def request_page(_):
+    user_query = str(request.args.get('user'))
+    dataset={"Page": "Request", "Message": f"successfully got the request for {user_query}", "Timestamp":time.time}
     json_dump = json.dumps(dataset)
 
     return json_dump
+
+if __name__ == "__main__":
+    app.run(port=7777)
