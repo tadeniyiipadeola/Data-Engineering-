c6efa453c02b558bdeb7d76da31c91a46d978796
Temi <stadeniy@cougarnet.uh.edu>
2022-01-28 15:07:54 -0600

    Working with Json data in jupyter Notebook

--- JSON/DataFrame - JSON.py
+++ JSON/DataFrame - JSON.py
@@ -4,17 +4,23 @@ people_string = '''
 {
     "people": [
         {
-            "name": "John Smith"
+            "name": "John Smith",
             "Phone": "615-555-7164",
             "eamils": ["johnsmith@bogusemail.com", "john.smith@work-place.com"], 
             "has_license":false
         },
         {
-            "name":"Jane Doe",
+            "name": "Jane Doe",
             "phone": "560-555-5153",
             "emails": null,
-            "has_license": True
+            "has_license": true
         }
     ]
 }
 '''
+
+data = json.loads(people_string)
+print(data)
+print(type(data['people']))
+for person in data['people']:
+    print(person['name'])
